131a6403cea01e4557e966a18eebf5d97699c889
ThaH3lper <craftopia@live.com>
2015-03-05 15:10:49 +0100

    Created Projekt *Yey

---
+++ desktop/src/com/StekStudio/Orb/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.StekStudio.Orb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.StekStudio.Orb.OrbGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 360;
+		config.height = 640;
+		new LwjglApplication(new OrbGame(), config);
+	}
+}
---
+++ ios/src/com/StekStudio/Orb/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.StekStudio.Orb;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.StekStudio.Orb.OrbGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new OrbGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
